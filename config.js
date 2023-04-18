1f6f118a46d16b832b52792629359a2938ca266f
NirvedGhose <76044657+NirvedGhose@users.noreply.github.com>
2023-04-18 13:17:12 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyAGkIq_DHO0Ooj7WINA9nZU9IKgNWHq0gM",
+    authDomain: "team-voting-97abd.firebaseapp.com",
+    projectId: "team-voting-97abd",
+    storageBucket: "team-voting-97abd.appspot.com",
+    messagingSenderId: "284772837192",
+    appId: "1:284772837192:web:160ebd0ef0a2c6814cf57d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
